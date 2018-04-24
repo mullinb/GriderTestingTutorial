5f5a881ca1dfce7c6d9858f750e2ba0e4a1e72bb
Brian Mullin <mullin.mm@gmail.com>
2018-04-24 08:44:41 +0200

    initial commit of testing tutorial course

---
+++ src/components/comment_list.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CommentList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        const list = this.props.comments.map(comment => <li key={comment}>{comment}</li>)
+        return (
+            <ul className="comment-list">
+                {list}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { comments: state.comments };
+}
+
+
+export default connect(mapStateToProps)(CommentList);
